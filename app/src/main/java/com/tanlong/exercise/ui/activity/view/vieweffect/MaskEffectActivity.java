8a0362b38cb50e66f674378fe89773200c4ffe67
tan long <tlaqcxb@163.com>
2016-10-25 14:11:04 +0800

    1. 完成通过WindowManager实现蒙版效果的练习

--- app/src/main/java/com/tanlong/exercise/ui/activity/view/vieweffect/MaskEffectActivity.java
+++ app/src/main/java/com/tanlong/exercise/ui/activity/view/vieweffect/MaskEffectActivity.java
@@ -2,7 +2,9 @@ package com.tanlong.exercise.ui.activity.view.vieweffect;
 
 import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -28,6 +30,9 @@ public class MaskEffectActivity extends BaseActivity {
     @Bind(R.id.tv_title)
     TextView tvTitle;
 
+    ImageView ivMask;
+    WindowManager windowManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,40 +40,60 @@ public class MaskEffectActivity extends BaseActivity {
         setContentView(R.layout.activity_mask_effect);
         ButterKnife.bind(this);
 
+        windowManager = getWindowManager();
         initView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    showMask();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 1000);//延迟一段时间显示，避免要显示弹框时，父View还没创建完毕，
+                 // 抛出android.view.WindowManager$BadTokenException异常
     }
 
     private void initView() {
         tvTitle.setText(R.string.view_effect_mask);
-        showMask();
+
+        ivMask = new ImageView(MaskEffectActivity.this);
+        ivMask.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        ivMask.setScaleType(ImageView.ScaleType.FIT_XY);
+        ivMask.setImageResource(R.mipmap.ic_mask);
+
+        ivMask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                windowManager.removeView(ivMask);
+            }
+        });
     }
 
     private void showMask() {
-        try {
-            // 动态初始化图层
-            ImageView ivMask = new ImageView(this);
-            ivMask.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT));
-            ivMask.setScaleType(ImageView.ScaleType.FIT_XY);
-            ivMask.setImageResource(R.mipmap.ic_mask);
+        LogTool.e(TAG, "showMask");
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+        // 设置显示的类型，
+        params.type = WindowManager.LayoutParams.TYPE_APPLICATION_PANEL;
+        // 设置显示格式
+        params.format = PixelFormat.RGBA_8888;
+        // 设置对齐方式
+        params.gravity = Gravity.LEFT | Gravity.TOP;
+        params.width = DisplayUtil.getDisplay(this).x;
+        params.height = DisplayUtil.getDisplay(this).y;
+        params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                        | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+                        | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
 
-            // 设置LayoutParams参数
-            WindowManager.LayoutParams params = new WindowManager.LayoutParams();
-            // 设置显示的类型，TYPE_PHONE指的是来电话的时候会被覆盖，其他时候会在最前端，显示位置在stateBar下面，其他更多的值请查阅文档
-            params.type = WindowManager.LayoutParams.TYPE_PHONE;
-            // 设置显示格式
-            params.format = PixelFormat.RGBA_8888;
-            // 设置对齐方式
-            params.gravity = Gravity.LEFT | Gravity.TOP;
-            // 设置宽高
-            params.width = DisplayUtil.getDisplay(this).x;
-            params.height = DisplayUtil.getDisplay(this).y;
-
-            getWindowManager().addView(ivMask, params);
-        } catch (Exception e) {
-            e.printStackTrace();
-            LogTool.e(TAG, e.getMessage());
-        }
+        windowManager.addView(ivMask, params);
     }
 
     @OnClick(R.id.iv_back)
