90b7587076f6509b4cf21c8b988fbd4b224171eb
tan long <tlaqcxb@163.com>
2016-10-29 15:37:27 +0800

    1. 添加DialogFragment大小控制

--- app/src/main/java/com/tanlong/exercise/ui/fragment/LoginDialogFragment.java
+++ app/src/main/java/com/tanlong/exercise/ui/fragment/LoginDialogFragment.java
@@ -1,8 +1,10 @@
 package com.tanlong.exercise.ui.fragment;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,5 +25,17 @@ public class LoginDialogFragment extends DialogFragment {
         getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
         View rootView = inflater.inflate(R.layout.dialog_login, container);
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            dialog.getWindow().setLayout((int)(displayMetrics.widthPixels * 0.75),
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
     }
 }
