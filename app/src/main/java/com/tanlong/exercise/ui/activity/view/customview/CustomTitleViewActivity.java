6b387f192b14811e5354391fb0d95c34f5e7eb7f
tan long <tlaqcxb@163.com>
2016-11-29 17:46:33 +0800

    1. 添加ShowTipsFragment，用于显示每个练习的知识要点

--- app/src/main/java/com/tanlong/exercise/ui/activity/view/customview/CustomTitleViewActivity.java
+++ app/src/main/java/com/tanlong/exercise/ui/activity/view/customview/CustomTitleViewActivity.java
@@ -1,11 +1,14 @@
 package com.tanlong.exercise.ui.activity.view.customview;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.tanlong.exercise.R;
 import com.tanlong.exercise.ui.activity.base.BaseActivity;
+import com.tanlong.exercise.ui.fragment.ShowTipsFragment;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -13,7 +16,6 @@ import butterknife.OnClick;
 
 
 /**
- *
  * Created by Administrator on 2016/5/15.
  */
 public class CustomTitleViewActivity extends BaseActivity {
@@ -21,6 +23,8 @@ public class CustomTitleViewActivity extends BaseActivity {
     ImageView mIvBack;
     @Bind(R.id.tv_title)
     TextView mTvitle;
+    @Bind(R.id.btn_help)
+    Button btnHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +38,17 @@ public class CustomTitleViewActivity extends BaseActivity {
 
     public void initView() {
         mTvitle.setText(R.string.custom_title_view);
+        btnHelp.setVisibility(View.VISIBLE);
     }
 
     @OnClick(R.id.iv_back)
     public void onBack() {
         finish();
+    }
+
+    @OnClick(R.id.btn_help)
+    public void onClick() {
+        ShowTipsFragment fragment = ShowTipsFragment.newInstance("测试");
+        fragment.show(getSupportFragmentManager(), "");
     }
 }
---
+++ app/src/main/java/com/tanlong/exercise/ui/fragment/ShowTipsFragment.java
@@ -0,0 +1,93 @@
+package com.tanlong.exercise.ui.fragment;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.widget.TextView;
+
+import com.tanlong.exercise.R;
+import com.tanlong.exercise.util.DisplayUtil;
+import com.tanlong.exercise.util.LogTool;
+import com.tanlong.exercise.util.VersionUtil;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+/**
+ * 显示提示对话框
+ * Created by 龙 on 2016/11/29.
+ */
+
+public class ShowTipsFragment extends DialogFragment {
+    private final String TAG = getClass().getSimpleName();
+    @Bind(R.id.tv_tips)
+    TextView tvTips;
+    public static final String SHOW_TIPS = "showtipsfragment";
+
+    public static ShowTipsFragment newInstance(String content) {
+        ShowTipsFragment fragment = new ShowTipsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(SHOW_TIPS, content);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        final View rootView = inflater.inflate(R.layout.dialog_show_tips, container);
+        ButterKnife.bind(this, rootView);
+
+        if (getArguments() != null) {
+            String content = getArguments().getString(SHOW_TIPS, "暂无内容");
+            tvTips.setText(content);
+        }
+
+        rootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (VersionUtil.hasJellyBean()) {
+                    rootView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                } else {
+                    rootView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                }
+
+                int rootHeitht = rootView.getHeight();
+                int halfScreenHeight = (int) (DisplayUtil.getDisplay(getContext()).y * 0.5);
+                Dialog dialog = getDialog();
+                LogTool.e(TAG, "rootHeitht is " + rootHeitht + " halfScreenHeight is " + halfScreenHeight);
+                if (rootHeitht < halfScreenHeight) {//小于屏幕高度的一半
+                    if (dialog != null) {
+                        DisplayMetrics displayMetrics = new DisplayMetrics();
+                        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                        dialog.getWindow().setLayout((int)(displayMetrics.widthPixels * 0.75),
+                                ViewGroup.LayoutParams.WRAP_CONTENT);
+                    }
+                } else {
+                    if (dialog != null) {
+                        DisplayMetrics displayMetrics = new DisplayMetrics();
+                        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                        dialog.getWindow().setLayout((int)(displayMetrics.widthPixels * 0.75), halfScreenHeight);
+                    }
+                }
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+}
