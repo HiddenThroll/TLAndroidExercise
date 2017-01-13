5a1b38250891585dd192f8d2b63d0d1e57ffd6c1
tan long <tlaqcxb@163.com>
2017-01-13 17:39:27 +0800

    1. 自定义LeftDrawerLayout，实现手指滑动菜单

--- app/src/main/java/com/tanlong/exercise/ui/view/customviewgroup/LeftDrawerLayout2.java
+++ app/src/main/java/com/tanlong/exercise/ui/view/customviewgroup/LeftDrawerLayout2.java
@@ -37,58 +37,55 @@ public class LeftDrawerLayout2 extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        try {
-            LogTool.e(TAG, "onMeasure");
-            int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-            int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-            setMeasuredDimension(widthSize, heightSize);
+        LogTool.e(TAG, "onMeasure");
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        setMeasuredDimension(widthSize, heightSize);
 
-            // 测量菜单View
-            View leftMenuView = getChildAt(1);
-            MarginLayoutParams lp = (MarginLayoutParams) leftMenuView.getLayoutParams();
-            int menuWidthSpec = getChildMeasureSpec(widthMeasureSpec, lp.leftMargin + lp.rightMargin,
-                    lp.width);
-            int menuHeightSpec = getChildMeasureSpec(heightMeasureSpec, lp.topMargin + lp.bottomMargin,
-                    lp.height);
-            leftMenuView.measure(menuWidthSpec, menuHeightSpec);
+        /**
+         * getChildMeasureSpec() 在measureChildren中最难的部分：找出传递给child的MeasureSpec。
+         * 目的是结合父view的MeasureSpec与子view的LayoutParams信息去找到最好的结果
+         * （也就是说子view的确切大小由两方面共同决定：1.父view的MeasureSpec 2.子view的LayoutParams属性）
+         *
+         * @param spec 父view的详细测量值(MeasureSpec)
+         * @param padding view当前尺寸的的内边距和外边距(padding,margin)
+         * @param childDimension child在当前尺寸下的布局参数宽高值(LayoutParam.width,height)
+         */
 
-            // 测量内容View
-            View contentView = getChildAt(0);
-            lp = (MarginLayoutParams) contentView.getLayoutParams();
-            int contentWidthSpec = getChildMeasureSpec(widthMeasureSpec, lp.leftMargin + lp.rightMargin,
-                    lp.width);
-            int contentHeightSpec = getChildMeasureSpec(heightMeasureSpec, lp.topMargin + lp.bottomMargin,
-                    lp.height);
-            contentView.measure(contentWidthSpec, contentHeightSpec);
+        // 测量菜单View
+        mMenuView = getChildAt(1);
+        MarginLayoutParams lp = (MarginLayoutParams) mMenuView.getLayoutParams();
+        int menuWidthSpec = getChildMeasureSpec(widthMeasureSpec, lp.leftMargin + lp.rightMargin,
+                lp.width);
+        int menuHeightSpec = getChildMeasureSpec(heightMeasureSpec, lp.topMargin + lp.bottomMargin,
+                lp.height);
+        mMenuView.measure(menuWidthSpec, menuHeightSpec);
 
-            mMenuView = leftMenuView;
-            mContentView = contentView;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        // 测量内容View
+        mContentView = getChildAt(0);
+        lp = (MarginLayoutParams) mContentView.getLayoutParams();
+        int contentWidthSpec = getChildMeasureSpec(widthMeasureSpec, lp.leftMargin + lp.rightMargin,
+                lp.width);
+        int contentHeightSpec = getChildMeasureSpec(heightMeasureSpec, lp.topMargin + lp.bottomMargin,
+                lp.height);
+        mContentView.measure(contentWidthSpec, contentHeightSpec);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         LogTool.e(TAG, "onLayout");
-        try {
-            //布局内容
-            MarginLayoutParams lp = (MarginLayoutParams) mMenuView.getLayoutParams();
-            mMenuView.layout(lp.leftMargin, lp.topMargin, lp.leftMargin + mMenuView.getMeasuredWidth(),
-                    lp.topMargin + mMenuView.getMeasuredHeight());
+        //布局内容
+        MarginLayoutParams lp = (MarginLayoutParams) mContentView.getLayoutParams();
+        mContentView.layout(lp.leftMargin, lp.topMargin, lp.leftMargin + mContentView.getMeasuredWidth(),
+                lp.topMargin + mContentView.getMeasuredHeight());
 
-            //布局菜单
-            lp = (MarginLayoutParams) mMenuView.getLayoutParams();
-            int menuWidth = mMenuView.getMeasuredWidth();
-            int menuLeft = -menuWidth;
-            mMenuView.layout(menuLeft, lp.topMargin, menuLeft + menuWidth,
-                    lp.topMargin + mMenuView.getMeasuredHeight());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        //布局菜单
+        lp = (MarginLayoutParams) mMenuView.getLayoutParams();
+        int menuWidth = mMenuView.getMeasuredWidth();
+        int menuLeft = -menuWidth;
+        LogTool.e(TAG, "menuLeft is " + menuLeft);
+        mMenuView.layout(menuLeft, lp.topMargin, menuLeft + menuWidth,
+                lp.topMargin + mMenuView.getMeasuredHeight());
     }
 
     @Override
@@ -107,7 +104,8 @@ public class LeftDrawerLayout2 extends ViewGroup {
             @Override
             public int clampViewPositionHorizontal(View child, int left, int dx) {
                 // 水平移动距离 [-child.getWidth()，0]
-                return Math.max(child.getWidth(), Math.min(left, 0));
+                int result = Math.max(-child.getWidth(), Math.min(left, 0));
+                return result;
             }
 
             @Override
@@ -117,6 +115,7 @@ public class LeftDrawerLayout2 extends ViewGroup {
 
             @Override
             public void onEdgeDragStarted(int edgeFlags, int pointerId) {
+                // 因为开始的时候mMenuView不可见，通过captureChildView对mMenuView进行捕获
                 mDragerHelper.captureChildView(mMenuView, pointerId);
             }
         });
@@ -134,5 +133,18 @@ public class LeftDrawerLayout2 extends ViewGroup {
     public boolean onTouchEvent(MotionEvent event) {
         mDragerHelper.processTouchEvent(event);
         return true;
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new MarginLayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+    }
+
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
+
+    protected LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
+        return new MarginLayoutParams(p);
     }
 }
