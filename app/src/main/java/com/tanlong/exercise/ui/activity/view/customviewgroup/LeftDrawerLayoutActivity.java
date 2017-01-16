a0b3655f62a9e4cdc9abd67c72509dff82d39d01
tan long <tlaqcxb@163.com>
2017-01-16 13:43:17 +0800

    1. LeftDrawerLayoutActivity添加知识要点

--- app/src/main/java/com/tanlong/exercise/ui/activity/view/customviewgroup/LeftDrawerLayoutActivity.java
+++ app/src/main/java/com/tanlong/exercise/ui/activity/view/customviewgroup/LeftDrawerLayoutActivity.java
@@ -2,19 +2,22 @@ package com.tanlong.exercise.ui.activity.view.customviewgroup;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.tanlong.exercise.R;
 import com.tanlong.exercise.ui.activity.base.BaseActivity;
-import com.tanlong.exercise.ui.view.customviewgroup.LeftDrawerLayout;
+import com.tanlong.exercise.ui.fragment.ShowTipsFragment;
 import com.tanlong.exercise.ui.view.customviewgroup.LeftDrawerLayout2;
 import com.tanlong.exercise.util.LogTool;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import static android.R.attr.fragment;
 
 /**
  * LeftDrawerLayout测试Activity
@@ -30,6 +33,8 @@ public class LeftDrawerLayoutActivity extends BaseActivity {
     LeftDrawerLayout2 mLeftDrawerLayout;
     @Bind(R.id.rl_container)
     RelativeLayout rlContainer;
+    @Bind(R.id.btn_help)
+    Button btnHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +44,8 @@ public class LeftDrawerLayoutActivity extends BaseActivity {
         ButterKnife.bind(this);
 
         mTvTitle.setText(R.string.left_drawer_layout);
+        btnHelp.setVisibility(View.VISIBLE);
+
     }
 
     @OnClick({R.id.iv_back, R.id.rl_container})
@@ -54,4 +61,14 @@ public class LeftDrawerLayoutActivity extends BaseActivity {
         }
     }
 
+    @OnClick(R.id.btn_help)
+    public void onClick() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("1. 自定义ViewGroup，实现子View的测量与布局\n")
+                .append("2. 根据上一小节内容实现菜单View跟随手指滑动\n")
+                .append("3. 使用ViewDragHelper.smoothSlideViewTo(int left, int top)->View.invalidate()->View.computeScroll()->ViewDragHelper.continueSettling(boolean deferCallbacks)->View.invalidate()实现平滑移动效果，即打开/关闭菜单\n")
+                .append("4. 覆写ViewDragHelper.onViewReleased()方法，根据MenuView距父布局的左边距判断打开/关闭菜单");
+        ShowTipsFragment fragment = ShowTipsFragment.newInstance(stringBuilder.toString());
+        fragment.show(getSupportFragmentManager(), "");
+    }
 }
