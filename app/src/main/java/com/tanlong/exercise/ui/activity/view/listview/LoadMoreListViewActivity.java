36da506df6b8e01248abe73625512f0c224bcfff
tan long <tlaqcxb@163.com>
2017-01-24 14:48:18 +0800

    1. 修改LoadMoreListViewActivity，解决初始化时Item未铺满整个ListView仍加载更多的bug

--- app/src/main/java/com/tanlong/exercise/ui/activity/view/listview/LoadMoreListViewActivity.java
+++ app/src/main/java/com/tanlong/exercise/ui/activity/view/listview/LoadMoreListViewActivity.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AbsListView;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 
 import com.tanlong.exercise.R;
 import com.tanlong.exercise.ui.activity.base.BaseActivity;
+import com.tanlong.exercise.util.LogTool;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +23,6 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
- *
  * Created by 龙 on 2016/6/28.
  */
 public class LoadMoreListViewActivity extends BaseActivity {
@@ -67,7 +68,11 @@ public class LoadMoreListViewActivity extends BaseActivity {
 
             @Override
             public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                if (firstVisibleItem + visibleItemCount == totalItemCount) {// 到达底部
+                int selectItemHeight = getSelectItemHeight(mLvCategory, firstVisibleItem, visibleItemCount, totalItemCount);
+                LogTool.e(TAG, "ListView height is " + view.getHeight());
+                LogTool.e(TAG, "getSelectItemHeight is " + selectItemHeight);
+                if (firstVisibleItem + visibleItemCount == totalItemCount && // 到达底部
+                        selectItemHeight > view.getHeight()) {// 并且已加载Item高度大于ListView规定高度
                     if (!mIsLoadingMore) {// 没有加载更多
                         mIsLoadingMore = true;
                         // 显示加载更多
@@ -76,7 +81,7 @@ public class LoadMoreListViewActivity extends BaseActivity {
                             @Override
                             public void run() {
                                 int size = mItems.size();
-                                for (int i = size; i < size + 10; i++) {
+                                for (int i = size; i < size + 5; i++) {
                                     mItems.add("item " + i);
                                 }
                                 mAdapter.notifyDataSetChanged();
@@ -95,5 +100,27 @@ public class LoadMoreListViewActivity extends BaseActivity {
     @OnClick(R.id.iv_back)
     public void onBack() {
         finish();
+    }
+
+    public int getSelectItemHeight(ListView listView, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        Adapter adapter = listView.getAdapter();
+        if (adapter == null || adapter.getCount() == 0) {
+            return 0;
+        }
+        int allItemHeight = 0;
+        for (int i = firstVisibleItem; i < firstVisibleItem + visibleItemCount; i++) {
+            if (i >= totalItemCount) {
+                break;
+            }
+            View itemView = adapter.getView(i, null, listView);
+            itemView.measure(0, 0);
+            allItemHeight += itemView.getMeasuredHeight();
+        }
+
+        int allDividerHeight = listView.getDividerHeight() * (visibleItemCount - 1);
+        if (allDividerHeight < 0) {
+            allDividerHeight = 0;
+        }
+        return allItemHeight + allDividerHeight;
     }
 }
