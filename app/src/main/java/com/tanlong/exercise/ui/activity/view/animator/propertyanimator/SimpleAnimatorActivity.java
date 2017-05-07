d418a7ca98f6df7e01242a222281dcf4dedb0b4b
谭龙 <tanl@dgj.com>
2017-05-07 18:57:40 +0800

    1.  基本属性动画练习添加知识要点

--- app/src/main/java/com/tanlong/exercise/ui/activity/view/animator/propertyanimator/SimpleAnimatorActivity.java
+++ app/src/main/java/com/tanlong/exercise/ui/activity/view/animator/propertyanimator/SimpleAnimatorActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import com.orhanobut.logger.Logger;
 import com.tanlong.exercise.R;
 import com.tanlong.exercise.ui.activity.base.BaseActivity;
+import com.tanlong.exercise.ui.fragment.dialog.ShowTipsFragment;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -61,7 +62,21 @@ public class SimpleAnimatorActivity extends BaseActivity {
     }
 
     private void showTips() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("属性动画基本使用:\n")
+                .append("1. ObjectAnimator.ofFloat(Object target, String propertyName, Float... values)\n")
+                .append("1.1 target是操作的对象, 这里是View\n")
+                .append("1.2 propertyName是操作的属性, 该属性必须有get和set方法, 系统通过反射操作属性\n")
+                .append("1.3  Float... values是变长参数, 是属性变化的取值过程\n")
+                .append("2. AnimatorSet, 对同一个对象的多个属性作用多种动画, 实现精确的顺序控制\n")
+                .append("2.1 playTogether() 同时作用\n")
+                .append("2.2 playSequentially() 顺序执行\n")
+                .append("2.3 play(animator1).with(animator2) 同时作用,与playTogether一样\n")
+                .append("2.4 play(animator1).before(animator2) 先animator1, 再animator2\n")
+                .append("2.5 play(animator1).after(animator2) 先animator2, 再animator1");
 
+        ShowTipsFragment fragment = ShowTipsFragment.newInstance(stringBuilder.toString());
+        fragment.show(getSupportFragmentManager(), "");
     }
 
     private void startAnimator() {
@@ -73,8 +88,8 @@ public class SimpleAnimatorActivity extends BaseActivity {
         ObjectAnimator rotateAnim = ObjectAnimator.ofFloat(tvSimpleAnimator, "rotation", 0, 360);
         animatorSet = new AnimatorSet();
         animatorSet.setDuration(1000 * 10);
-        animatorSet.play(translationAnim).with(scaleXAnim).with(scaleYAnim)
-                .after(alphaAnim).before(rotateAnim);
+        animatorSet.play(translationAnim).with(scaleXAnim).with(scaleYAnim)//同时位移和缩放
+                .after(alphaAnim).before(rotateAnim);   //先透明度, 后旋转
         animatorSet.start();
 
     }
