17337c91ef64e2c5aef185af23fb68204c79f4a1
谭龙 <tanl@dgj.com>
2017-07-17 21:24:35 +0800

    1.  Activity转场动画添加知识要点

--- app/src/main/java/com/tanlong/exercise/ui/activity/view/animator/activityanimator/ActivityAnimatorCategoryActivity.java
+++ app/src/main/java/com/tanlong/exercise/ui/activity/view/animator/activityanimator/ActivityAnimatorCategoryActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import com.tanlong.exercise.R;
 import com.tanlong.exercise.ui.activity.base.BaseActivity;
+import com.tanlong.exercise.ui.fragment.dialog.ShowTipsFragment;
 import com.tanlong.exercise.util.ToastHelp;
 
 import butterknife.Bind;
@@ -45,7 +46,8 @@ public class ActivityAnimatorCategoryActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animator_category);
         ButterKnife.bind(this);
-
+        tvTitle.setText(R.string.activity_transition);
+        btnHelp.setVisibility(View.VISIBLE);
     }
 
     @OnClick({R.id.iv_back, R.id.btn_help, R.id.btn_transition_explode, R.id.btn_transition_slide,
@@ -74,7 +76,38 @@ public class ActivityAnimatorCategoryActivity extends BaseActivity {
     }
 
     private void showTips() {
+        StringBuilder stringBuilder = new StringBuilder();
 
+        stringBuilder.append("Activity转场动画\n")
+                .append("1. 分类\n")
+                .append("1.1 进入动画, 决定Activity中所有View怎么进入屏幕\n")
+                .append("1.2 退出动画, 决定Activity中所有View怎么退出屏幕\n")
+                .append("1.3 共享动画, 决定两个Activity之间怎么共享它们的View\n")
+                .append("2. 效果\n")
+                .append("2.1 进入和退出动画包括\n")
+                .append("2.1.1 explode分解, 从屏幕中间进或出, 移动视图\n")
+                .append("2.1.2 slide滑动, 从屏幕边缘进或出, 移动视图\n")
+                .append("2.1.3 fade淡入淡出, 改变屏幕上视图的不透明度来添加或移除视图\n")
+                .append("2.2 共享元素动画包括\n")
+                .append("2.2.1 changeBounds 改变View的布局边界\n")
+                .append("2.2.2 changeClipBounds 裁剪View边界\n")
+                .append("2.2.3 changeTransform 改变View的缩放比例和旋转角度\n")
+                .append("2.2.4 changeImageTransform 改变图片的大小和缩放比例\n")
+                .append("3. 使用\n")
+                .append("3.1 进入和退出\n")
+                .append("3.1.1 ActivityA\n")
+                .append("通过 startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(this).toBundle())启动ActivityB\n")
+                .append("3.1.2 ActivityB\n")
+                .append("在setContentVIew()之前\n")
+                .append("(1) getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITION)允许使用转场动画\n")
+                .append("(2) getWindow().setEnterTransition(new Explode()) 设置进入动画\n")
+                .append("(3) getWindow().setExitTransition(new Explode()) 设置退出动画\n")
+                .append("3.2 共享元素\n")
+                .append("3.2.1 布局文件\n")
+                .append("在ActivityA和ActivityB的布局文件中的共享元素(View)设置 android:transitionName 为同一个字符串,指定共享元素\n")
+                .append("3.2.2 ActivityA\n")
+                .append("通过 startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(this, sharedView, sharedName))启动ActivityB");
+        ShowTipsFragment.newInstance(stringBuilder.toString()).show(getSupportFragmentManager(), "");
     }
 
     private void startActivityTransitionAnimator(int flag) {
--- app/src/main/java/com/tanlong/exercise/ui/activity/view/animator/activityanimator/TransitionActivity.java
+++ app/src/main/java/com/tanlong/exercise/ui/activity/view/animator/activityanimator/TransitionActivity.java
@@ -5,10 +5,18 @@ import android.os.Bundle;
 import android.transition.Explode;
 import android.transition.Fade;
 import android.transition.Slide;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.tanlong.exercise.R;
 import com.tanlong.exercise.ui.activity.base.BaseActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2017/6/26.
@@ -24,6 +32,15 @@ public class TransitionActivity extends BaseActivity {
     public final static int MODE_SHARE = 4;
 
     private final int DURATION_TIME = 500;
+    @Bind(R.id.iv_back)
+    ImageView ivBack;
+    @Bind(R.id.tv_title)
+    TextView tvTitle;
+    @Bind(R.id.btn_help)
+    Button btnHelp;
+    @Bind(R.id.iv_share)
+    ImageView ivShare;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,5 +75,18 @@ public class TransitionActivity extends BaseActivity {
         }
         // 所有操作在设置内容视图之前
         setContentView(R.layout.activity_transition);
+        ButterKnife.bind(this);
+        tvTitle.setText(R.string.activity_transition);
+    }
+
+    @OnClick({R.id.iv_back, R.id.btn_help})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.btn_help:
+                break;
+        }
     }
 }
