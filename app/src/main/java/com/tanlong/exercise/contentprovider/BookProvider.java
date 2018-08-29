442e41a60e2b6e4d8412eb0fc68e984f9fccf6db
long.tan@w_oasis.com <long.tan@w-oasis.com>
2018-08-29 15:18:02 +0800

    1.BookProvider通过SqLite数据库提供添加和查找数据功能

--- app/src/main/java/com/tanlong/exercise/contentprovider/BookProvider.java
+++ app/src/main/java/com/tanlong/exercise/contentprovider/BookProvider.java
@@ -2,25 +2,70 @@ package com.tanlong.exercise.contentprovider;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 
 import com.orhanobut.logger.Logger;
+import com.tanlong.exercise.database.DbOpenHelper;
 
+/**
+ * @author 龙
+ */
 public class BookProvider extends ContentProvider {
+
+    public static final String AUTHORITIES = "com.tanlong.exercise.book.provider";
+
+    public static final Uri BOOK_CONTENT_URI = Uri.parse("content://" + AUTHORITIES + "/book");
+    public static final Uri USER_CONTENT_URI = Uri.parse("content://" + AUTHORITIES + "/user");
+
+    public static final int BOOK_URI_CODE = 1;
+    public static final int USER_URI_CODE = 2;
+
+    private SQLiteDatabase mDb;
+
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    //关联 URI 和 URI CODE
+    static {
+        uriMatcher.addURI(AUTHORITIES, "book", BOOK_URI_CODE);
+        uriMatcher.addURI(AUTHORITIES, "user", USER_URI_CODE);
+    }
+
     @Override
     public boolean onCreate() {
-        Logger.e("BookProvider onCreate thread is " + Thread.currentThread().getName() );
-        return false;
+        Logger.e("BookProvider onCreate thread is " + Thread.currentThread().getName());
+        mDb = new DbOpenHelper(getContext()).getWritableDatabase();
+        return true;
+    }
+
+    private String getTableName(Uri uri) {
+        String tableName = "";
+        switch (uriMatcher.match(uri)) {
+            case BOOK_URI_CODE:
+                tableName = DbOpenHelper.BOOK_TABLE_NAME;
+                break;
+            case USER_URI_CODE:
+                tableName = DbOpenHelper.USER_TABLE_NAME;
+                break;
+            default:
+                break;
+        }
+        return tableName;
     }
 
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        Logger.e("BookProvider query thread is " + Thread.currentThread().getName() );
-        return null;
+        Logger.e("BookProvider query thread is " + Thread.currentThread().getName());
+        String table = getTableName(uri);
+        if (TextUtils.isEmpty(table)) {
+            throw new IllegalArgumentException("Unsupport Uri: " + uri);
+        }
+        return mDb.query(table, projection, selection, selectionArgs, null, null, sortOrder, null);
     }
 
     @Nullable
@@ -32,7 +77,13 @@ public class BookProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
+        String table = getTableName(uri);
+        if (TextUtils.isEmpty(table)) {
+            throw new IllegalArgumentException("Unsupport Uri: " + uri);
+        }
+        mDb.insert(table, null, values);
+        getContext().getContentResolver().notifyChange(uri, null);
+        return uri;
     }
 
     @Override
--- app/src/main/java/com/tanlong/exercise/ui/activity/contentprovider/BookProviderActivity.java
+++ app/src/main/java/com/tanlong/exercise/ui/activity/contentprovider/BookProviderActivity.java
@@ -1,29 +1,50 @@
 package com.tanlong.exercise.ui.activity.contentprovider;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.databinding.DataBindingUtil;
-import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+import com.orhanobut.logger.Logger;
 import com.tanlong.exercise.R;
+import com.tanlong.exercise.aidl.Book;
+import com.tanlong.exercise.contentprovider.BookProvider;
 import com.tanlong.exercise.databinding.ActivityBookProviderBinding;
 import com.tanlong.exercise.ui.activity.base.BaseActivity;
+
+import java.util.Random;
 
 /**
  * @author 龙
  */
 public class BookProviderActivity extends BaseActivity {
     ActivityBookProviderBinding binding;
-    private final String AUTHORITIES = "com.tanlong.exercise.book.provider";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_book_provider);
-
-        Uri uri = Uri.parse("content://" + AUTHORITIES);
-        getContentResolver().query(uri, null, null, null,null);
-        getContentResolver().query(uri, null, null, null,null);
-        getContentResolver().query(uri, null, null, null,null);
+        binding.setActivity(this);
     }
 
+    public void addBook() {
+        ContentValues contentValues = new ContentValues();
+        int id = new Random().nextInt(900000);
+        contentValues.put("_id", id);
+        contentValues.put("name", "Android开发艺术探索" + id);
+        getContentResolver().insert(BookProvider.BOOK_CONTENT_URI, contentValues);
+    }
+
+    public void queryBooks() {
+        Cursor cursor = getContentResolver().query(BookProvider.BOOK_CONTENT_URI, new String[]{"_id", "name"},
+                null, null, null);
+        while (cursor.moveToNext()) {
+            String name = cursor.getString(1);
+            Book book = new Book(name, 1);
+            Logger.e("book is " + new Gson().toJson(book));
+        }
+        cursor.close();
+    }
 }
