2c4fc46485f1a0d8f97b23a1487ec03f5954a690
long.tan@w_oasis.com <long.tan@w-oasis.com>
2019-02-14 14:39:22 +0800

    1.插件化,完善简单加载模式

--- app/src/main/java/com/tanlong/exercise/ui/activity/plugin/SimpleLoadDexActivity.java
+++ app/src/main/java/com/tanlong/exercise/ui/activity/plugin/SimpleLoadDexActivity.java
@@ -8,6 +8,7 @@ import com.orhanobut.logger.Logger;
 import com.tanlong.exercise.R;
 import com.tanlong.exercise.databinding.ActivitySimpleLoadDexBinding;
 import com.tanlong.exercise.ui.activity.base.BaseActivity;
+import com.tanlong.exercise.util.ToastHelp;
 
 import java.io.File;
 import java.lang.reflect.Method;
@@ -38,7 +39,7 @@ public class SimpleLoadDexActivity extends BaseActivity {
     public void loadDex() {
         //找到要加载的dex文件
         File dexFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath()
-                + File.separator + "wxlz" + File.separator + "LoadDex.dex");
+                + File.separator + "wxlz" + File.separator + "target.dex");
         // 无法直接从外部路径加载.dex文件，需要指定APP内部路径作为缓存目录（.dex文件会被解压到此目录）
         File dexOutputDir = this.getDir("dex", 0);
         Logger.e("dexOutputDir is " + dexOutputDir.getAbsolutePath());
@@ -48,13 +49,15 @@ public class SimpleLoadDexActivity extends BaseActivity {
         //通过反射方式获取加载类
         Class loadDexClass = null;
         try {
-            loadDexClass = dexClassLoader.loadClass("com.tanlong.exercise.ui.activity.plugin.content.LoadDex");
+            loadDexClass = dexClassLoader.loadClass(
+                    "com.tanlong.exercise.ui.activity.plugin.content.LoadDex");
             Method getContent = loadDexClass.getMethod("getContent");
             getContent.setAccessible(true);
             loadContent = (String) getContent.invoke(loadDexClass.newInstance(), null);
             Logger.e("加载Dex成功, content is " + loadContent);
         } catch (Exception e) {
             e.printStackTrace();
+            ToastHelp.showShortMsg(this, "发生错误 " + e.getMessage());
         }
     }
 }
